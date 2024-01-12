6e79732f16be865442557df17c7c02af8c83d3a4
jamazzz <duartecraft69@gmail.com>
2024-01-12 19:49:58 +0000

    refactor: added dashboard

--- access/login.php
+++ access/login.php
@@ -1,11 +1,29 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . "/turno_1_v1/acesso_bd_t1.php");
+include($_SERVER['DOCUMENT_ROOT'] . "/storeify/bd.php");
+session_start();
 
-// $consulta = "SELECT * FROM users WHERE user = '".$_POST["username"]."'";
+// Email Validation
+$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+if ($email === false) {
+    echo "Email Inválido";
+}
+$pass = password_hash($_POST['password2'], PASSWORD_BCRYPT);
 
-// $resultado = mysqli_query($conexao, $consulta);
 
-// $numero_de_linhas = mysqli_num_rows($resultado);
-echo ($_POST['email']);
-echo ("<br>");
-echo ($_POST['password']);
+if (strpos($_POST['details'], '@') !== false) {
+    echo "This is an email";
+} else {
+    echo "This is not a valid email";
+}
+
+$nameuser = $_POST['fname'] . " " . $_POST['lname'];
+$userid = "INSERT INTO users_ids (name) VALUES ('{$nameuser}')";
+
+$resultado = mysqli_query($connect, $userid);
+
+if ($resultado) {
+    header("Location: access.php");
+    exit();
+} else {
+    echo "Error: " . mysqli_error($conexao);
+}
