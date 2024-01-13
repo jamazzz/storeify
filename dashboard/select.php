3678c49155eff61b64e3dc14c2730726adfdf407
jamazzz <duartecraft69@gmail.com>
2024-01-13 16:17:47 +0000

    feat: added select project page

---
+++ dashboard/select.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/select.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/45266fece3.js" crossorigin="anonymous"></script>
+</head>
+<div class="container-fluid">
+
+    <header class="page-title">
+        <div class="row no-gutters">
+            <div class="col-12 col-md-6 my-2">
+                <h1>Your Projects</h1>
+            </div>
+            <!-- <div class="col-12 col-md-6 text-md-right my-2">
+                <a href="https://creator.tebex.io/creator-segments" class="btn btn-primary" style="margin: 10px;">Create Project</a>
+            </div> -->
+        </div>
+    </header>
+
+    <div class="row">
+        <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
+            <div class="card h-100" style="border: 1px solid #d9d9d9 ;">
+                <div class="card-header">
+                    <div class="float-left">
+                        <i class="fa-solid fa-cart-shopping"></i> ‎ Shop Name
+                    </div>
+                    <div class="float-right">
+                        <a href="https://creator.tebex.io/webstores/1317549/delete" class="btn btn-danger"><i class="fa fa-times"></i></a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <p class="mb-2">Project Package</p>
+                    <p class="mb-0"><a href="pages/samples/error-404.html" target="_blank">View webstore</a></p>
+                    <a href="#" class="btn text-primary disabled btn-outline-primary mt-4">Logged in</a>
+                    <a href="https://creator.tebex.io/settings/transfer?1317549" class="btn text-secondary mt-4">Transfer</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
+            <a class="passive-link" href="https://creator.tebex.io/creator-segments">
+                <div class="card h-100" style="border: 1.5px dashed #d9d9d9 ;">
+                    <div class="card-body text-center d-flex align-items-center">
+                        <div class="mx-auto">
+                            <i class="fa-solid fa-circle-plus" style="font-size: 30px;"></i>
+                            <h3>Create webstore</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+
+</div>
+
+</body>
+
+</html>
