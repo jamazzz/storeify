d7194a04cffaa0cf0538852e1c083b6f03925ff2
jamazzz <duartecraft69@gmail.com>
2024-01-15 13:07:32 +0000

    refactor: readme and query

---
+++ query.sql
@@ -0,0 +1,128 @@
+CREATE TABLE users_perms (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    level INT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    elimination_date DATETIME,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    permission_level INT NOT NULL,
+    creation_date DATETIME NOT NULL,
+    attempts INT NOT NULL DEFAULT 5,
+    deleted BOOLEAN DEFAULT FALSE,
+    elimination_date DATETIME,
+    FOREIGN KEY (permission_level) REFERENCES users_perms(id)
+);
+
+CREATE TABLE users_data (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    login DATETIME NOT NULL,
+    logout DATETIME NOT NULL
+);
+
+CREATE TABLE users_ids (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    nif VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    postal_code VARCHAR(255) NOT NULL,
+    birth_date DATETIME,
+    gender INT NOT NULL,
+    nationality VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (gender) REFERENCES gender(id)
+);
+
+CREATE TABLE gender (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    gender VARCHAR(255) NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE,
+    elimination_date DATETIME NOT NULL
+);
+
+CREATE TABLE products (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    deleted BOOLEAN DEFAULT FALSE,
+    price DECIMAL(10, 2) NOT NULL,
+    section_id INT NOT NULL,
+    FOREIGN KEY (section_id) REFERENCES sections(id)
+);
+
+CREATE TABLE packages (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    duration INT NOT NULL,
+    creation_date DATETIME NOT NULL,
+    elimination_date DATETIME,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE websites (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    owner INT NOT NULL,
+    members INT NOT NULL,
+    package_id INT,
+    subscription_date DATETIME,
+    expiration_date DATETIME,
+    favicon VARCHAR(255),
+    FOREIGN KEY (package_id) REFERENCES packages(id)
+);
+
+CREATE TABLE members (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    memberid INT NOT NULL,
+    websiteid INT NOT NULL
+);
+
+CREATE TABLE sections (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    number INT,
+    section_type VARCHAR(255),
+    website_id INT,
+    FOREIGN KEY (website_id) REFERENCES websites(id)
+);
+
+CREATE TABLE discount_coupons (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    store_name VARCHAR(255) NOT NULL,
+    discount DECIMAL(5,2) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    used_count INT DEFAULT 0,
+    max_usage INT NOT NULL,
+    creation_date DATETIME NOT NULL,
+    elimination_date DATETIME,
+    deleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (store_name) REFERENCES websites(name)
+);
+
+CREATE TABLE clients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    elimination_date DATETIME,
+    login_attempts INT NOT NULL DEFAULT 5,
+    deleted BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE shopping_carts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    website_id INT,
+    client_id INT,
+    -- 
+    FOREIGN KEY (website_id) REFERENCES websites(id),
+    FOREIGN KEY (client_id) REFERENCES clients(id)
+);
