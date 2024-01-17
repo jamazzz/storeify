efc8a4fdb10f6ed12440cca55a16f09f2a5f0c9e
jamazzz <duartecraft69@gmail.com>
2024-01-17 12:44:12 +0000

    refactor: improved select.php

--- dashboard/select.php
+++ dashboard/select.php
@@ -27,20 +27,22 @@
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . "/storeify/bd.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/storeify/check.php");
-$projects = "SELECT websiteid FROM members WHERE memberid = '" . $_SESSION['userid'] . "'";
+// include($_SERVER['DOCUMENT_ROOT'] . "/storeify/check.php");
+$projects = "SELECT websiteid FROM members WHERE memberid = '" . 1 . "'";
+// $projects = "SELECT websiteid FROM members WHERE memberid = '" . $_SESSION['userid'] . "'";
 $result = mysqli_query($connect, $projects);
 $total = mysqli_num_rows($result);
-// $total = 0;
+$total = 10;
 
 if ($total == 0) {
     mysqli_close($connect);
     header('Location: /storeify/dashboard/new.php');
     exit();
 }
-if ($total == 1) {
-    echo '<div class="row">
-    <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
+echo '<div class="row" style="margin-left: 35px;">';
+for ($i = 0; $i < $total; $i++) {
+    echo '
+    <div class="col-11 col-md-11 col-lg-11 col-xl-11 mb-5">
         <div class="card h-100" style="border: 1px solid #d9d9d9 ;">
             <div class="card-header">
                 <div class="float-left">
@@ -57,60 +59,22 @@ if ($total == 1) {
                 <a href="pages/samples/error-404.html" class="btn text-secondary mt-4">Transfer</a>
             </div>
         </div>
-    </div>
-
-    <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
+    </div>';
+}
+echo ' <div class="col-11 col-md-11 col-lg-11 col-xl-11 mb-5">
         <a class="passive-link" href="pages/samples/error-404.html">
             <div class="card h-100" style="border: 1.5px dashed #d9d9d9 ;">
+            <p class="mb-2">‎</p>
+            <p class="mb-2">‎</p>
                 <div class="card-body text-center d-flex align-items-center">
                     <div class="mx-auto">
                         <i class="fa-solid fa-circle-plus" style="font-size: 30px;"></i>
                         <h3>Create webstore</h3>
+                        <p class="mb-2">‎</p>
                     </div>
                 </div>
             </div>
         </a>
     </div>
 </div>';
-}
-if ($total == 2) {
-    echo '<div class="row">
-    <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
-        <div class="card h-100" style="border: 1px solid #d9d9d9 ;">
-            <div class="card-header">
-                <div class="float-left">
-                    <i class="fa-solid fa-cart-shopping" style="padding-top: 17px;"></i> ‎ Shop Name
-                </div>
-                <div class="float-right">
-                    <a href="pages/samples/error-404.html" class="btn btn-danger"><i class="fa fa-times"></i></a>
-                </div>
-            </div>
-            <div class="card-body">
-                <p class="mb-2">Project Package</p>
-                <p class="mb-0"><a href="pages/samples/error-404.html" target="_blank">View webstore</a></p>
-                <a href="#" class="btn text-primary disabled btn-outline-primary mt-4">Logged in</a>
-                <a href="pages/samples/error-404.html" class="btn text-secondary mt-4">Transfer</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-md-6 col-lg-6 col-xl-6 row-mb">
-        <div class="card h-100" style="border: 1px solid #d9d9d9 ;">
-            <div class="card-header">
-                <div class="float-left">
-                    <i class="fa-solid fa-cart-shopping" style="padding-top: 17px;"></i> ‎ Shop Name
-                </div>
-                <div class="float-right">
-                    <a href="pages/samples/error-404.html" class="btn btn-danger"><i class="fa fa-times"></i></a>
-                </div>
-            </div>
-            <div class="card-body">
-                <p class="mb-2">Project Package</p>
-                <p class="mb-0"><a href="pages/samples/error-404.html" target="_blank">View webstore</a></p>
-                <a href="#" class="btn text-primary disabled btn-outline-primary mt-4">Logged in</a>
-                <a href="pages/samples/error-404.html" class="btn text-secondary mt-4">Transfer</a>
-            </div>
-        </div>
-    </div>
-</div>';
-}
 ?>
