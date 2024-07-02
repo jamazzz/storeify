f63359447e3e8ba344ad4036894fa9b2a2069876
jamazzz <duartecraft69@gmail.com>
2024-07-02 19:00:45 +0100

    refactor: createCategory

--- createPackage.php
+++ createPackage.php
@@ -1,25 +1,42 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/storeify/essencial.php';
-$insert = "INSERT INTO products (name, price, category_id, path, order, website_id) VALUES ('" . $_POST['name'] . "', '" . $_POST['price'] . "', '" . $_POST['category_id'] . "', '" . $_SESSION['thumbnail'] . "', '" . $_POST['order'] . "', '" . $_SESSION['currentwebsite'] . "')";
-mysqli_query($connection, $insert);
-$id = mysqli_insert_id($connection);
 
-$fileTmpPath = $_SESSION['thumbnail'];
-$uploadFileDir = $_SERVER['DOCUMENT_ROOT'] . '/storeify/store/productsimg/';
-$dest_path = $uploadFileDir;
-if (!is_dir($uploadFileDir)) {
-  mkdir($uploadFileDir, 0777, true);
-}
-move_uploaded_file($fileTmpPath, $dest_path);
-$fileType = pathinfo($dest_path, PATHINFO_EXTENSION);
-$newFileName = $id . "." . $fileType;
-$newFilePath = $uploadFileDir . $newFileName;
-rename($dest_path, $newFilePath);
+$name = $_POST['name'];
+$price = $_POST['price'];
+$category = $_POST['category'];
+$description = $_POST['description'];
 
-$zipPath = $_SESSION['zip'];
-$zipDir = $_SERVER['DOCUMENT_ROOT'] . '/storeify/store/' . $_SESSION['currentwebsite'] . '/';
-if (!is_dir($zipDir)) {
-  mkdir($zipDir, 0777, true);
+function moveUploadedFile($tmp_name, $upload_dir, $name)
+{
+  $target_path = $upload_dir . $name;
+  return move_uploaded_file($tmp_name, $target_path);
 }
-$newZipPath = $zipDir . basename($zipPath);
-rename($zipPath, $newZipPath);
+
+$file_name = $_FILES['zip']['name'];
+$file_tmp_name = $_FILES['zip']['tmp_name'];
+$upload_dir_files = $_SERVER['DOCUMENT_ROOT'] . '/storeify/store/websites/' . $_SESSION['websitename'] . '/';
+if (!is_dir($upload_dir_files)) {
+  if (!mkdir($upload_dir_files, 0777, true)) {
+    die('Failed to create folders...');
+  }
+}
+move_uploaded_file($file_tmp_name, $upload_dir_files . $file_name);
+$file_path = htmlspecialchars($upload_dir_files . $file_name);
+
+
+$order_query = "SELECT MAX(`order`) as max_order FROM products WHERE category_id = '$category'";
+$result = mysqli_query($connect, $order_query);
+$row = mysqli_fetch_assoc($result);
+$order = isset($row['max_order']) ? $row['max_order'] + 1 : 1;
+
+$sql = "INSERT INTO products (name, price, category_id, description, path, `order`) VALUES ('$name', '$price', '$category', '$description', '$file_path', $order)";
+$result = mysqli_query($connect, $sql);
+$id = mysqli_insert_id($connect);
+
+$thumbnail_name = $_FILES['thumbnail']['name'];
+$thumbnail_tmp_name = $_FILES['thumbnail']['tmp_name'];
+$upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/storeify/store/productsimg/';
+moveUploadedFile($thumbnail_tmp_name, $upload_dir, $id . '.png');
+
+header('Location: /storeify/dashboard/packages/packages.php');
+exit();
